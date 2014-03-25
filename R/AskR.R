e303f816948e50830dd0a81782d2e7e1e0bb4602
Dason Kurkiewicz <dasonk@iastate.edu>
2014-03-25 16:46:20 -0500

    Insults the user the attempt after they try to ask too many questions.  Users are jerks.

--- R/AskR.R
+++ R/AskR.R
@@ -19,13 +19,23 @@
               "Outlook not so good",
               "Very doubtful")
 
+.AskR_User_isajerk_count <- 0
+
 AskR<-function(question="Will this package be a great success?")
 {
   
+  if(.AskR_User_isajerk_count > 0){
+    .AskR_User_isajerk_count <<- .AskR_User_isajerk_count - 1
+    stop("You were a jerk so now I insult you", "\n", "Your mother was a hamster and your father smelt of elderberries")
+  }
+  
   # Only allow one question.  Berate the user if they try to ask more
   if(length(question) > 1){
-    stop("Only ask a single question you jerk")
+    .AskR_User_isajerk_count <<- .AskR_User_isajerk_count + 1
+    stop("Only ask a single question you jerk. Now I shall be forced to insult you")
   }
+  
+  
   
   response<-sample(1:length(.Responses),1)
   
